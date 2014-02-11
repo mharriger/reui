508684428a93b09efe3252846de4ec96a7b54943
Matt Harriger <mharriger@gmail.com>
2014-02-11 10:13:52 -0600

    Direct draw to screen bitmap.
    Reduce the number of copies/overhead in drawing the screen by having Box draw directly to it's Screen's bitmap.

--- reui/Box.py
+++ reui/Box.py
@@ -11,29 +11,30 @@ class Box:
     width = width of box
     height = height of box
     '''
-    def __init__(self, width, height, border_flags=0):
-        self._bitmap = Bitmap(width, height, 'y') 
+    def __init__(self, screen, width, height, border_flags=0):
         self.width = width
         self.height = height
         self.border_flags = border_flags
+        self.font = arial_16
+        self._screen = screen
         
     def draw_border(self):
         if self.border_flags & reui.BORDER_TOP:
             for x in range(self.width - 1):
-                self._bitmap.draw_pixel(x, 0)
+                self._screen.draw_pixel(self, x, 0)
         if self.border_flags & reui.BORDER_BOTTOM:
             for x in range(self.width - 1):
-                self._bitmap.draw_pixel(x, self.height - 1)
+                self._screen.draw_pixel(self, x, self.height - 1)
         if self.border_flags & reui.BORDER_LEFT:
             for y in range(self.height - 1):
-                self._bitmap.draw_pixel(0, y)
+                self._screen.draw_pixel(self, 0, y)
         if self.border_flags & reui.BORDER_RIGHT:
             for y in range(self.height - 1):
-                self._bitmap.draw_pixel(self.width - 1, y)
+                self._screen.draw_pixel(self, self.width - 1, y)
     
     def refresh(self):
         dispatcher.send(signal=reui.SGL_BOX_UPDATE, sender=self)
 
     def draw_text(self, x, y, string, font=arial_16, inverse=False):
-        self._bitmap.draw_text(x, y, string, font, inverse)
+        self._screen.draw_text(self, x, y, string, font, inverse)
 
